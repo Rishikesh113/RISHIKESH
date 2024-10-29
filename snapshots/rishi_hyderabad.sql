a2740133b01707b79abcc33ad0ac61245eee4339
Rishi Kesh <mayakuntla.rishikesh1994@gmail.com>
2024-10-29 06:30:56 +0000

    commit

--- snapshots/rishi_hyderabad.sql
+++ snapshots/rishi_hyderabad.sql
@@ -1,7 +1,9 @@
 { % snapshot rishi_hyderabad % }
-{{ config(startegy='Timestamp',
+{{ 
+    config(startegy='Timestamp',
 unique_key='id',
-updated_at='updated_at')
+updated_at='updated_at'
+)
 }}
 
 select * from {{source('datafeed_shared_schema','Rishi_hyd')}}
