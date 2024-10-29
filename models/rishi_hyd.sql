e5ae86fefa8099ae7216d719e5fbf545e97b4caa
Rishi Kesh <mayakuntla.rishikesh1994@gmail.com>
2024-10-29 17:37:50 +0000

    commit

--- models/rishi_hyd.sql
+++ models/rishi_hyd.sql
@@ -1,7 +1,7 @@
 {{config (materialized='table') }}
 
 with rishi as (
-    select * from {{source('datafeed_shared_schema','rishi_hyd')}}
+    select * from {{source('datafeed_shared_schema','RISHI_HYD')}}
 )
 
 select * from rishi 
--- snapshots/rishi_nayab.sql
+++ snapshots/rishi_nayab.sql
@@ -1,12 +1,13 @@
-{% snapshot rishi_hyderabad %}
+{% snapshot rishi_nayab %}
 {{ 
     config(Target_schema ='snapshots',
     strategy='timestamp',
 unique_key='id',
-updated_at='updated_at'
+updated_at='updated_at',
+invalidate_hard_deletes=True
 )
 }}
 
 select * from {{source('datafeed_shared_schema','RISHI_HYD')}}
 
-{% endsnapshot %}     
+{% endsnapshot %}      
